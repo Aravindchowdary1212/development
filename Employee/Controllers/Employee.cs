50f632426fdb6b955a95c7bcb058d48d6bec81d4
Aravindchowdary1212 <aravind944000@gmail.com>
2025-06-11 06:48:39 +0530

    the checking

---
+++ Employee/Controllers/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Employee : ControllerBase
+    {
+
+        [HttpGet ,Route("employee")]
+        public IActionResult Employes()
+        {
+            try
+            {
+                string Employee = "";
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+            return Ok("nbggfgg");
+        }
+    }
+}
