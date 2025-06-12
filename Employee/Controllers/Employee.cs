a7f87e8bebdce67e9a824dc85fc02132d8e6a808
Aravindchowdary1212 <aravind944000@gmail.com>
2025-06-12 19:55:14 +0530

    changes

--- Employee/Controllers/Employee.cs
+++ Employee/Controllers/Employee.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using InterfaceEmployee;
+using log4net;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Configuration;
 
 namespace Employee.Controllers
 {
@@ -7,19 +10,34 @@ namespace Employee.Controllers
     [ApiController]
     public class Employee : ControllerBase
     {
+        private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        string classname = "Employee";
+        private IEmployes iEmployes = null;
+        private IConfiguration _Configuration = null;
+
+        public Employee(IConfiguration configuration,IEmployes employes)
+        {
+            iEmployes= employes;
+            _Configuration = configuration;
+
+        }
 
         [HttpGet ,Route("employee")]
         public IActionResult Employes()
         {
+            _log.Info("class:"+classname+"employee Start");
             string Employee=string.Empty;
             try
             {
-                Employee = "the way how to bild the project123";
+                Employee = iEmployes.employe();
             }
             catch(Exception e)
             {
+                _log.Error("class:" + classname + "employee "+ e);
                 return BadRequest(e.Message);
             }
+            _log.Info("class:" + classname + "employee end");
+
             return Ok(Employee);
         }
 
@@ -28,14 +46,18 @@ namespace Employee.Controllers
         public IActionResult Employes1()
         {
             string Employee = string.Empty;
+            _log.Info("class:" + classname + "employee1 Start");
+
             try
             {
                 Employee = "Iam Check Branch Pullinh Requsted";
             }
             catch (Exception e)
             {
+                _log.Error("class:" + classname + "employee " + e);
                 return BadRequest(e.Message);
             }
+            _log.Info("class:" + classname + "employee1 end");
             return Ok(Employee);
         }
     }
---
+++ Employes/Employe.cs
@@ -0,0 +1,42 @@
+﻿using InterfaceEmployee;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employes
+{
+    public class Employe: IEmployes
+    {
+        private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private IEmployesDao iEmployesDao = null;
+        string classname = "Employee";
+        private IConfiguration _Configuration = null;
+
+        public Employe(IEmployesDao employesDao,IConfiguration configuration)
+        {
+            iEmployesDao = employesDao;
+            _Configuration = configuration;
+        }
+        public string employe()
+        {
+            _log.Info("class:" + classname + "employe Start");
+            string value = string.Empty;
+            try
+            {
+                value=iEmployesDao.employe();
+            }
+            catch (Exception ex)
+            {
+                _log.Error("class:" + classname + "employe " + ex);
+                throw ex;
+            }
+            _log.Info("class:" + classname + "employe end");
+
+            return value;
+        }
+
+    }
+}
